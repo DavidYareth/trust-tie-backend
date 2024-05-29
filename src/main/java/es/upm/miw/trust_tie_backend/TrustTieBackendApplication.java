c61ce1884beccaa90f225dee81a7029949a1ab24
DavidYareth <davidyareth@gmail.com>
2024-05-29 19:17:46 +0200

    Initial commit

---
+++ src/main/java/es/upm/miw/trust_tie_backend/TrustTieBackendApplication.java
@@ -0,0 +1,13 @@
+package es.upm.miw.trust_tie_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustTieBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustTieBackendApplication.class, args);
+	}
+
+}
