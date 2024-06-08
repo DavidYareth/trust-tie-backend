bbfb7e6d0469bdb8f57d2e35c73968b4b1dac817
DavidYareth <davidyareth@gmail.com>
2024-06-08 11:00:11 +0200

    Added CORSConfiguration

---
+++ src/main/java/es/upm/miw/trust_tie_backend/configurations/CORSConfiguration.java
@@ -0,0 +1,31 @@
+package es.upm.miw.trust_tie_backend.configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfiguration implements WebMvcConfigurer {
+    private String[] allowedOrigins;
+
+    @Autowired
+    public CORSConfiguration(@Value("http://localhost:4200") String allowedOrigins) {
+        this.setAllowedOrigins(allowedOrigins);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("*")
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+    private void setAllowedOrigins(String allowedOrigins) {
+        this.allowedOrigins = allowedOrigins.split(",");
+    }
+}
