752bf13e6814a8bb43b2f8801001439542e5c735
DavidYareth <davidyareth@gmail.com>
2024-06-20 20:06:31 +0200

    Fixed UserResourceIT

--- src/test/java/es/upm/miw/trust_tie_backend/api/resources/UserResourceIT.java
+++ src/test/java/es/upm/miw/trust_tie_backend/api/resources/UserResourceIT.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.*;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class UserResourceIT {
 
     @Autowired
